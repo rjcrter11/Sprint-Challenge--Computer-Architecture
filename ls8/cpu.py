b9d31444ca1feab1b5773ab11bd5967cd16244d7
Rachel <rjcrter11@gmail.com>
2020-07-24 10:58:37 -0500

    cleans up and comments code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,10 +1,9 @@
 """CPU functionality."""
 
 import sys
-from datetime import datetime, timedelta
 import time
 
-# ops
+# Instructions
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
@@ -14,7 +13,15 @@ PUSH = 0b01000101
 POP = 0b01000110
 PRA = 0b01001000
 
-# ALU ops
+# PC mutating Instructions
+CALL = 0b01010000
+RET = 0b00010001
+IRET = 0b00010011
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+
+# ALU Operations
 ADD = 0b10100000
 SUB = 0b10100001
 MUL = 0b10100010
@@ -30,15 +37,6 @@ XOR = 0b10101011
 SHL = 0b10101100
 SHR = 0b10101101
 
-# PC mutators
-CALL = 0b01010000
-RET = 0b00010001
-INT = 0b01010010
-IRET = 0b00010011
-JMP = 0b01010100
-JEQ = 0b01010101
-JNE = 0b01010110
-
 # Stack pointer
 SP = 7
 # Interrupt status
@@ -52,13 +50,13 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
-        self.reg = [0] * 8
-        self.pc = 0
-        self.fl = 0
+        self.ram = [0] * 256  # RAM
+        self.reg = [0] * 8  # Registers
+        self.pc = 0  # Program counter
+        self.fl = 0  # CMP flag
         self.running = True
-        self.reg[SP] = 0xf4
-        self.ie = True
+        self.reg[SP] = 0xf4  # Initialized stack pointer
+        self.ie = True  # Interrupt flag
 
         self.dispatchtable = {
             HLT: self.handle_HLT,
@@ -77,12 +75,14 @@ class CPU:
             JNE: self.handle_JNE,
         }
 
+    # RAM functions
     def ram_read(self, address):
         return self.ram[address]
 
     def ram_write(self, address, value):
         self.ram[address] = value
 
+    # Instruction handlers
     def handle_HLT(self):
         self.running = False
 
@@ -100,6 +100,7 @@ class CPU:
         self.reg[a] = self.ram_read(self.reg[SP])
         self.reg[SP] += 1
 
+    # Instruction handlers that set pc directly
     def handle_CALL(self, a):
         self.reg[SP] -= 1
         self.ram_write(self.reg[SP], self.pc + 2)
@@ -112,25 +113,7 @@ class CPU:
     def handle_LD(self, a, b):
         self.reg[a] = self.ram_read(self.reg[b])
 
-    def check_interrupts(self):
-        masked_interrupts = self.reg[IM] & self.reg[IS]
-
-        for i in range(8):
-            interrupt_happened = ((masked_interrupts >> i) & 1) == 1
-
-            if interrupt_happened:
-                self.ie = False   # disable interrupts
-                self.reg[IS] = 0  # clear IS
-                self.reg[SP] -= 1  # push pc onto stack
-                self.ram_write(self.reg[SP], self.pc)
-                self.reg[SP] -= 1  # push flag onto stack
-                self.ram_write(self.reg[SP], self.fl)
-                for i in range(0, 7):  # push R0 - R6
-                    self.reg[SP] -= 1
-                    self.ram_write(self.reg[SP], self.reg[i])
-
-                self.pc = self.ram[0xf8]
-
+    # Conditional jump instructions
     def handle_JMP(self, a):
         self.pc = self.reg[a]
 
@@ -146,12 +129,7 @@ class CPU:
         else:
             self.pc += 2
 
-    def handle_PRA(self, a):
-        print(chr(self.reg[a]), end='', flush=True)
-
-    def handle_ST(self, a, b):
-        self.ram_write(self.reg[a], self.reg[b])
-
+    # Interrupt instructions
     def handle_IRET(self):
 
         for i in range(6, -1, -1):  # Pop R6-R0
@@ -161,29 +139,35 @@ class CPU:
         self.reg[SP] += 1
         self.pc = self.ram_read(self.reg[SP])  # pop return address as pc
         self.reg[SP] += 1
-        self.ie = True  # re-enable interrupt
+        self.ie = True  # re-enable interrupts
 
-    def load(self):
-        """Load a program into memory."""
-        file_name = f'examples/{sys.argv[1]}.ls8'
-        try:
-            address = 0
-            with open(file_name) as f:
-                for line in f:
+    def handle_PRA(self, a):
+        print(chr(self.reg[a]), end='', flush=True)
 
-                    command = line.split('#')[0].strip()
+    def handle_ST(self, a, b):
+        self.ram_write(self.reg[a], self.reg[b])
 
-                    if command == '':
-                        continue
+    # Interrupt helper function
+    def check_interrupts(self):
+        masked_interrupts = self.reg[IM] & self.reg[IS]
 
-                    instruction = int(command, 2)
-                    self.ram_write(address, instruction)
-                    address += 1
+        for i in range(8):
+            interrupt_happened = ((masked_interrupts >> i) & 1) == 1
 
-        except FileNotFoundError:
-            print(f'{sys.argv[0]} : {sys.argv[1]} file was not found')
-            sys.exit()
+            if interrupt_happened:
+                self.ie = False   # disable interrupts
+                self.reg[IS] = 0  # clear IS register
+                self.reg[SP] -= 1  # push pc onto stack
+                self.ram_write(self.reg[SP], self.pc)
+                self.reg[SP] -= 1  # push flag onto stack
+                self.ram_write(self.reg[SP], self.fl)
+                for i in range(0, 7):  # push R0 - R6 onto stack
+                    self.reg[SP] -= 1
+                    self.ram_write(self.reg[SP], self.reg[i])
 
+                self.pc = self.ram[0xf8]  # set PC to handler address
+
+    # ALU Operations
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
         # flags: 00000LGE
@@ -241,6 +225,7 @@ class CPU:
             result = self.reg[reg_a] ^ self.reg[reg_b]
             self.reg[reg_a] = result
 
+    # CPU State Tracer
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -261,24 +246,54 @@ class CPU:
 
         print()
 
+    # File Loader
+    def load(self):
+        """Load a program into memory."""
+        file_name = f'examples/{sys.argv[1]}.ls8'
+        try:
+            address = 0
+            with open(file_name) as f:
+                for line in f:
+
+                    command = line.split('#')[0].strip()
+
+                    if command == '':
+                        continue
+
+                    instruction = int(command, 2)
+                    self.ram_write(address, instruction)
+                    address += 1
+
+        except FileNotFoundError:
+            print(f'{sys.argv[0]} : {sys.argv[1]} file was not found')
+            sys.exit()
+
     def run(self):
         """Run the CPU."""
 
         start_time = time.time()
         while self.running:
 
+            # Check if interrupter flag is enabled
+            # then check for interrupts
             if self.ie:
                 self.check_interrupts()
 
+            # Check time for interrupt
+            # After 1 second, set IS register and restart clock
             check_time = time.time()
             if check_time - start_time > 1:
                 self.reg[IS] = 0b00000001
                 start_time = time.time()
 
+            # Instruction register and operands
             IR = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
+            # Check for regular instructions,
+            # direct set instructions,
+            # and ALU operations
             instructions = (IR >> 6) + 1
             direct_set_instructions = ((IR >> 4) & 0b1) == 1
             alu_op = ((IR >> 5) & 0b1) == 1
